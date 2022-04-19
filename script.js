8b769e70388afeb28f7e616b1822fbe0eaee5f42
bransonpetty <bransonpetty99@msn.com>
2022-04-18 22:20:08 -0600

    Updated CSS Theme and added dark mode

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+/*script.js file has the main purpose of managing 
+consistent elements and their functionality. The
+tabMapping function along with the if statements 
+determine which tabs are allowed responsive elements
+such as links and style alteration with mouse hover.
+
+Changes include altering color scheme for dark mode,
+default theme on load, and altering cover image for
+dark mode. Needs to maintain theme upon switching pages
+*/
+
+
 let currTab;
 let homeTab = document.getElementById("home");
 let roomsTab = document.getElementById("rooms");
@@ -6,94 +18,122 @@ let mapTab = document.getElementById("map");
 let aboutTab = document.getElementById("about");
 let html = document.getElementsByTagName('html');
 let tabBox = document.getElementsByClassName('tabs');
-let tabs = [homeTab, roomsTab, activitiesTab, mapTab, aboutTab]
+let styleTag = document.querySelector("link[href='style.css']");
+let light = document.getElementById("themeLight");
+let dark = document.getElementById("themeDark");
+let tabs = [homeTab, roomsTab, activitiesTab, mapTab, aboutTab];
 
-function tabMapping(currTab) {
+let coverPhoto = document.getElementById("cover");
+let currCover = "cover photo.webp";
+let currTheme = "style.css";
+styleTag.setAttribute("href", currTheme);
+
+
+let primaryColor = "#4E96EA";
+let secondaryColor = "#0156B9";
+
+light.addEventListener("click", function(){
+    currTheme = "style.css"
+    styleTag.setAttribute("href", currTheme)
+    console.log('hi')
+    primaryColor = "#4E96EA";
+    secondaryColor = "#0156B9";
+    currCover = "cover photo.webp"
+    tabStyle()
+})
+
+dark.addEventListener("click", function(){
+    currTheme = "styleOverride.css"
+    styleTag.setAttribute("href", currTheme)
+    console.log(styleTag)
+    primaryColor = "#34373B";
+    secondaryColor = "#7147e6"
+    currCover = "nightCover.jpeg"
+    tabStyle()
+})
+
+function tabMapping(currTab) { //gives all tab elements responsive styling except for current tab
     for(let i = 0; i < 5; i++) {
         if(i != currTab) {
+            tabs[i].style.backgroundColor = secondaryColor;
             tabs[i].addEventListener("mouseenter", function() {
                 tabs[i].style.backgroundColor = "white";
-                tabs[i].style.color = "#0156B9";
+                tabs[i].style.color = secondaryColor;
             })
             tabs[i].addEventListener("mouseleave", function() {
-                tabs[i].style.backgroundColor = "#0156B9";
+                tabs[i].style.backgroundColor = secondaryColor;
                 tabs[i].style.color = "white";
             })
         }
     }
 };
-// console.log(tabs[1].style);
-
-// homeTab.setAttribute("href", "index.html");
-// roomsTab.setAttribute("href", "rooms.html");
 
 
-if(window.location.pathname == "/") {
+
+if(styleTag.getAttribute("href") == "style.css") {
+    primaryColor = "#4E96EA";
+    tabStyle()
+}
+
+if(styleTag.getAttribute("href") == "styleOverride.css") {
+    primaryColor = "#34373B";
+    console.log("hi")
+}
+
+if(window.location.pathname == "/") { //This is to declare the default tab upon navigation
     window.location.pathname = "/index.html"
 }
 
-if(window.location.pathname == "/index.html") {
-    homeTab.style.backgroundColor = "#4E96EA";
-    roomsTab.setAttribute("href", "rooms.html");
-    activitiesTab.setAttribute("href", "activities.html");
-    mapTab.setAttribute("href", "map.html");
-    aboutTab.setAttribute("href", "about.html");
-    tabMapping(0);
-}
-if(window.location.pathname == "/rooms.html") {
-    roomsTab.style.backgroundColor = "#4E96EA";
-    homeTab.setAttribute("href", "index.html");
-    activitiesTab.setAttribute("href", "activities.html");
-    mapTab.setAttribute("href", "map.html");
-    aboutTab.setAttribute("href", "about.html");
-    tabMapping(1);
+function tabStyle() {    
+    if(window.location.pathname == "/index.html") { //home tab
+        homeTab.style.backgroundColor = primaryColor;
+        roomsTab.setAttribute("href", "rooms.html");
+        activitiesTab.setAttribute("href", "activities.html");
+        mapTab.setAttribute("href", "map.html");
+        aboutTab.setAttribute("href", "about.html");
+        coverPhoto.setAttribute("src", currCover)
+        tabMapping(0);
+    }
+    if(window.location.pathname == "/rooms.html") {//rooms tab
+        roomsTab.style.backgroundColor = primaryColor;
+        homeTab.setAttribute("href", "index.html");
+        activitiesTab.setAttribute("href", "activities.html");
+        mapTab.setAttribute("href", "map.html");
+        aboutTab.setAttribute("href", "about.html");
+        tabMapping(1);
 
-    let king = document.getElementById("king");
-    let queen = document.getElementById("queen");
-    king.addEventListener("click", function(){
-        king.innerHTML = "All done!"
-    })
-    queen.addEventListener("click", function(){
-        queen.innerHTML = "All done!"
-    })
+        let king = document.getElementById("king");
+        let queen = document.getElementById("queen");
+        king.addEventListener("click", function(){
+            king.innerHTML = "All done!"
+        })
+        queen.addEventListener("click", function(){
+            queen.innerHTML = "All done!"
+        })
+    }
+    if(window.location.pathname == "/activities.html") {//activites tab
+        console.log("activity page")
+        activitiesTab.style.backgroundColor = primaryColor;
+        homeTab.setAttribute("href", "index.html");
+        roomsTab.setAttribute("href", "rooms.html");
+        mapTab.setAttribute("href", "map.html");
+        aboutTab.setAttribute("href", "about.html");
+        tabMapping(2);
+    }
+    if(window.location.pathname == "/map.html") {//map tab
+        mapTab.style.backgroundColor = primaryColor;
+        roomsTab.setAttribute("href", "rooms.html");
+        activitiesTab.setAttribute("href", "activities.html");
+        homeTab.setAttribute("href", "index.html");
+        aboutTab.setAttribute("href", "about.html");
+        tabMapping(3);
+    }
+    if(window.location.pathname == "/about.html") {//about tab
+        aboutTab.style.backgroundColor = primaryColor;
+        roomsTab.setAttribute("href", "rooms.html");
+        activitiesTab.setAttribute("href", "activities.html");
+        homeTab.setAttribute("href", "index.html");
+        mapTab.setAttribute("href", "map.html");
+        tabMapping(4);
+    }
 }
-if(window.location.pathname == "/activities.html") {
-    console.log("activity page")
-    activitiesTab.style.backgroundColor = "#4E96EA";
-    homeTab.setAttribute("href", "index.html");
-    roomsTab.setAttribute("href", "rooms.html");
-    mapTab.setAttribute("href", "map.html");
-    aboutTab.setAttribute("href", "about.html");
-    tabMapping(2);
-}
-if(window.location.pathname == "/map.html") {
-    mapTab.style.backgroundColor = "#4E96EA";
-    roomsTab.setAttribute("href", "rooms.html");
-    activitiesTab.setAttribute("href", "activities.html");
-    homeTab.setAttribute("href", "index.html");
-    aboutTab.setAttribute("href", "about.html");
-    tabMapping(3);
-}
-if(window.location.pathname == "/about.html") {
-    aboutTab.style.backgroundColor = "#4E96EA";
-    roomsTab.setAttribute("href", "rooms.html");
-    activitiesTab.setAttribute("href", "activities.html");
-    homeTab.setAttribute("href", "index.html");
-    mapTab.setAttribute("href", "map.html");
-    tabMapping(4);
-}
-
-    
-// // function mobile(){
-// //     document.getElementsByTagName('html').addEventListener(windo
-// // }
-
-// if(window.innerWidth < 985){
-//     // tabBox.style.gridTemplateColumns = ";
-//     tabBox.style.gridTemplateRows = "20% 20% 20% 20% 20%";
-// }
-// else {
-//     tabBox.style.gridTemplateColumns = "20% 20% 20% 20% 20%";
-
-// }
-// console.log(window.innerWidth)
